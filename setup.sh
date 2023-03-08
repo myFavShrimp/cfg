ca16e22fc413456e944fc8eed0b7b1bb04521219
myFavShrimp <myfavshrimp@tuta.io>
2023-03-08 10:53:31 +0100

    Added rust analyzer

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 sudo dnf copr enable atim/alacritty
 sudo dnf install alacritty
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain stable -y
 
 sudo dnf copr enable varlad/helix
 sudo dnf install helix
@@ -11,6 +11,8 @@ echo 'eval "$(starship init bash)"' >> ~/.bashrc
 
 cargo install --locked zellij
 sudo dnf install gitui
+
+rustup component add rust-analyzer
 
 cargo install fnm
 echo 'eval "$(fnm env --use-on-cd)"' >> ~/.bashrc
