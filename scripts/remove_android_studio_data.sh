087c816a7e125e8f146c467272961ab457952129
myFavShrimp <myfavshrimp@tuta.io>
2023-03-08 13:52:30 +0100

    Added android studio removal script

---
+++ scripts/remove_android_studio_data.sh
@@ -0,0 +1,8 @@
+rm -rf ~/Android
+rm -rf ~/.android
+rm -rf ~/.gradle
+rm -rf ~/.config/Google
+rm -rf ~/.cache/Google
+rm -rf ~/.config/Android\ Open\ Source\ Project
+rm -rf ~/.cache/Android\ Open\ Source\ Project
+rm -rf ~/.local/share/Android\ Open\ Source\ Project
