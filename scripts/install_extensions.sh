8a69983c816c6e95e2fcbb79709b18d17c8c6012
myFavShrimp <myfavshrimp@tuta.io>
2023-05-01 17:51:44 +0200

    Fixed extensions install script

--- scripts/install_extensions.sh
+++ scripts/install_extensions.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-EXTENSIONS=(cat resources/extensions | while read line; do echo -n "$line "; done)
+FILENAME='resources/gnome-extensions'
 
-for EXTENSION in "${EXTENSIONS[@]}"
-do
-    EXTENSION_ID=$(curl -s $i | grep -oP 'data-uuid="\K[^"]+')
-    VERSION_TAG=$(curl -Lfs "https://extensions.gnome.org/extension-query/?search=$EXTENSION_ID" | jq '.extensions[0] | .shell_version_map | map(.pk) | max')
+echo Start
+while read EXTENSION; do
+    EXTENSION_ID=$(curl -s $EXTENSION | grep -oP 'data-uuid="\K[^"]+')
+    echo "https://extensions.gnome.org/extension-query/?search=${EXTENSION_ID}"
+    VERSION_TAG=$(curl -Lfs "https://extensions.gnome.org/extension-query/?search=${EXTENSION_ID}" | jq '.extensions[0] | .shell_version_map | map(.pk) | max')
 
-    wget -O ${EXTENSION_ID}.zip "https://extensions.gnome.org/download-extension/${EXTENSION_ID}.shell-extension.zip?version_tag=$VERSION_TAG"
+    echo "https://extensions.gnome.org/download-extension/${EXTENSION_ID}.shell-extension.zip?version_tag=${VERSION_TAG}"
+    wget -O ${EXTENSION_ID}.zip "https://extensions.gnome.org/download-extension/${EXTENSION_ID}.shell-extension.zip?version_tag=${VERSION_TAG}"
     unzip ${EXTENSION_ID}.zip -d /usr/share/gnome-shell/extensions/${EXTENSION_ID}
 
     rm ${EXTENSION_ID}.zip
-done
+done < "$FILENAME"
