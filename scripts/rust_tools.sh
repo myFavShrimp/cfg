4bdff0e0088ee611e9d1123e57ef0b71505d1b37
myFavShrimp <myfavshrimp@tuta.io>
2024-03-20 18:23:33 +0100

    Updated rust-tools

--- scripts/rust_tools.sh
+++ scripts/rust_tools.sh
@@ -11,6 +11,10 @@ cargo install --locked lnkr
 cargo install --locked fd-find
 cargo install --locked nu
 cargo install --locked git-delta
+cargo install --locked trunk
+cargo install --locked sqlx-cli
+cargo install --locked cargo-watch
+
 rustup component add rust-analyzer
 
 # curl -sS https://starship.rs/install.sh | sh -s -s -- -y
